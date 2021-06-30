231966808dc6771ec3e22153a9d45c3721db850c
Henrique <henrique.nofv@gmail.com>
2021-06-29 23:46:45 -0300

    Added prettier

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,10 +1,10 @@
-import { Controller, Get } from "@overnightjs/core";
-import { Request, Response } from "express";
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 
 @Controller('forecast')
 export class ForecastController {
-    @Get('')
-    public getForecastForLoggedUser(_: Request, res: Response): void {
-        res.send([{}]);
-    }
+  @Get('')
+  public getForecastForLoggedUser(_: Request, res: Response): void {
+    res.send([{}]);
+  }
 }
--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,7 +1,7 @@
 describe('Beach forecast functional tests', () => {
-    it('should return a forecast with just a few times', async () => {
-        const { body, status } = await global.testRequest.get('/forecast');
-        expect(status).toBe(200);
-        expect(body).toEqual([{}])
-    })
-})
+  it('should return a forecast with just a few times', async () => {
+    const { body, status } = await global.testRequest.get('/forecast');
+    expect(status).toBe(200);
+    expect(body).toEqual([{}]);
+  });
+});
